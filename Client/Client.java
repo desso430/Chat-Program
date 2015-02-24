957ceb454199b47a7665b823896d281d349736d9
Desislav Hristov <desso430@gmial.com>
2015-02-24 14:30:23 +0200

    First working version (1.0)

--- Client/Client.java
+++ Client/Client.java
@@ -1,5 +1,60 @@
 package Client;
 
-public class Client {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ConnectException;
+import java.net.Socket;
+import java.util.Scanner;
 
+public class Client {
+	 public static void main(String args[]){
+	     Socket connection = null;
+	     Scanner socketIn = null;
+	     PrintWriter socketOut = null;
+	     String response = null;
+	     Scanner keyboardIn = new Scanner(System.in);
+	     int port = 1234;
+	     String host = "localhost";
+
+	     try{
+	       System.out.println("Connecting with server");
+	       try{
+	         connection = new Socket(host, port);
+	       }
+	       catch(ConnectException e){
+	         System.err.println(" Can not connect with the server!");
+	         return;
+	       }
+	       System.out.println("Connected with the server successfully!");
+
+	       socketIn = new Scanner(new BufferedReader(new InputStreamReader(connection.getInputStream())));
+	       socketOut = new PrintWriter(connection.getOutputStream(), true);
+	       do{	    	   
+		     String answer = socketIn.nextLine();
+		     System.out.println("Server : " + answer);
+		         
+	         socketOut.flush();
+	         System.out.print("Write message to server: ");
+	         response = keyboardIn.nextLine();
+	         socketOut.println(response.toLowerCase());
+	       }
+	       while (!response.equalsIgnoreCase("exit"));
+	       System.out.println(" Closing the connection...");
+	     }
+	     catch(IOException e) {
+	       e.printStackTrace();
+	     }
+	     finally{
+	       try{
+	         if(socketIn!=null) socketIn.close();
+	         if(socketOut!=null) socketOut.close();
+	         if(connection!=null) connection.close();
+	       }
+	       catch(IOException e){
+	         System.err.println(" Can not close the connection");
+	       }
+	     }
+	}
 }
--- Server/Server.java
+++ Server/Server.java
@@ -1,5 +1,64 @@
 package Server;
 
-public class Server {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
+public class Server {
+	private static final int PORT = 1234;
+
+	public static void main(String args[]) {
+		  Scanner keyboardIn = new Scanner(System.in);
+		    ServerSocket serverSocket = null;
+		    Socket connection = null;
+		    Scanner socketIn = null;
+	        String answer = null;
+	        String name = null;
+	        String response = null;
+		    PrintWriter socketOut = null;
+		    int port = PORT;
+
+		    while(true){
+		      try {
+		        serverSocket = new ServerSocket(port);
+		        System.out.println(" Waiting for someone to connect...");
+		        connection = serverSocket.accept();
+		        System.out.println(" Connected with:" + connection.getInetAddress().getHostName());
+
+		        socketOut = new PrintWriter(connection.getOutputStream(), true);
+		        socketIn = new Scanner(new BufferedReader(new InputStreamReader(connection.getInputStream())));
+
+		        name =  connection.getInetAddress().getHostName();
+		        do{	        	
+		          System.out.print("write message to " + name + ": ");
+				  response = keyboardIn.nextLine();
+				  socketOut.println(response.toLowerCase());
+				  
+		          socketOut.flush();
+		          answer = socketIn.nextLine();
+		          System.out.println( name + " say: " + answer);
+		        }
+		        while(!response.equalsIgnoreCase("exit"));
+		        System.out.println("Closing the connection with " + name);
+		      }
+		      catch(IOException e) {
+		        e.printStackTrace();
+		      }
+		      finally{
+		        try{
+		          if (socketIn!=null) socketIn.close();
+		          if (socketOut!=null) socketOut.close();
+		          if (connection!=null) connection.close();
+		          if (serverSocket!=null) serverSocket.close();
+		        }
+		        catch(IOException e){
+		          System.err.println(" Can not close the socket! ");
+		        }
+		      }
+		  }
+	 }
 }
