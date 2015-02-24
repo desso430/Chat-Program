7dea6201ecb3e198fbbb008b7945e74ea698f148
Desislav Hristov <desso430@gmial.com>
2015-02-24 19:34:27 +0200

    Optimized and export the code in different methods

--- Client/Client.java
+++ Client/Client.java
@@ -6,58 +6,78 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ConnectException;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class Client {
-	 private static final String HOST = "localhost";
-	private static final int PORT = 1234;
+	private static final String HOST = "localhost";
+	private static final int PORT = 1234;  
+	private static Scanner keyboardIn = new Scanner(System.in);
+	private static String nickname = null;
+	private static Socket connection = null;
+	private static Scanner socketIn = null;
+	private static PrintWriter socketOut = null;
+	private static String response = null;
 
 	public static void main(String args[]){
-	     Scanner keyboardIn = new Scanner(System.in);
-	     String nickname = null;
-	     Socket connection = null;
-	     Scanner socketIn = null;
-	     PrintWriter socketOut = null;
-	     String response = null;
-
+		
 	     try{
-	       System.out.println("Connecting with server");
-	         try{
-	           connection = new Socket(HOST, PORT);
-	         }
-	           catch(ConnectException e){
-	           System.err.println(" Can not connect with the server!");
-	           return;
-	         }
-	       System.out.println("Connected with the server successfully!");
-	       System.out.println(" Please enter your nickname!" );
-	       nickname = keyboardIn.nextLine();
-
-	       socketIn = new Scanner(new BufferedReader(new InputStreamReader(connection.getInputStream())));
-	       socketOut = new PrintWriter(connection.getOutputStream(), true);
+	       connectWithServer();         
+	       setNickname();
+	       setTheStreams();
+	       
 	       do{	    	   
-		     String answer = socketIn.nextLine();
-		     System.out.println(answer);
-		         
-	         socketOut.flush();
-	         response = keyboardIn.nextLine();
-	         socketOut.println(nickname + ": " + response.toLowerCase());
-	       }
-	       while (!response.equalsIgnoreCase("exit"));
-	       System.out.println(" Closing the connection...");
-	     }
-	     catch(IOException e) {
+		     receiveMessage();
+		     sendMessage();
+	       } while (!response.equalsIgnoreCase("exit"));    
+	     } catch(IOException e) {
 	       e.printStackTrace();
 	     }
 	     finally{
 	       try{
-	         if(socketIn!=null) socketIn.close();
-	         if(socketOut!=null) socketOut.close();
-	         if(connection!=null) connection.close();
-	       }
-	       catch(IOException e){
+	         closeTheSockets();
+	       } catch(IOException e){
 	         System.err.println(" Can not close the connection");
 	       }
 	     }
+	}
+
+	private static void connectWithServer() throws UnknownHostException, IOException {
+		System.out.println("Connecting with server");
+	     try{
+	         connection = new Socket(HOST, PORT);
+	       } catch(ConnectException e){
+	         System.err.println(" Can not connect with the server!");
+	         return;
+	       }
+	}
+
+	private static void closeTheSockets() throws IOException {
+	    System.out.println(" Closing the connection...");
+		if(socketIn!=null) socketIn.close();
+	    if(socketOut!=null) socketOut.close();
+		if(connection!=null) connection.close();
+	}
+
+	private static void sendMessage() {
+		socketOut.flush();
+		System.out.print("Me: ");
+		response = keyboardIn.nextLine();
+		socketOut.println(nickname + ": " + response.toLowerCase());
+	}
+
+	private static void receiveMessage() {
+		String answer = socketIn.nextLine();
+		System.out.println(answer);
+	}
+
+	private static void setNickname() {
+		System.out.println(" Please enter your nickname!" );
+	    nickname = keyboardIn.nextLine();
+	}
+
+	private static void setTheStreams() throws IOException {
+		socketIn = new Scanner(new BufferedReader(new InputStreamReader(connection.getInputStream())));
+	    socketOut = new PrintWriter(connection.getOutputStream(), true);
 	}
 }
--- Server/Server.java
+++ Server/Server.java
@@ -10,58 +10,77 @@ import java.util.Scanner;
 
 public class Server {
 	private static final int PORT = 1234;
+	private static ServerSocket serverSocket = null;
+	private static String nickname = null;
+	private static Socket connection = null;
+	private static Scanner socketIn = null;
+	private static String answer = null;
+	private static String computerName = null;
+	private static String response = null;
+	private static PrintWriter socketOut = null;
 
 	public static void main(String args[]) {
 		  Scanner keyboardIn = new Scanner(System.in);
-		  ServerSocket serverSocket = null;
-		  String nickname = null;
-		  Socket connection = null;
-		  Scanner socketIn = null;
-	      String answer = null;
-	      String name = null;
-	      String response = null;
-		  PrintWriter socketOut = null;
-
+		  
 		    while(true){
 		      try {
-		        serverSocket = new ServerSocket(PORT);
-		        System.out.println(" Waiting for someone to connect...");
-		        connection = serverSocket.accept();
-		        System.out.println(" Connected with:" + connection.getInetAddress().getHostName());
+		        WaitinForConnection();
+		        setingNickname(keyboardIn);		    
+		        setStreams();
 
-		        System.out.println(" Please enter your nickname!" );
-			    nickname = keyboardIn.nextLine();
-			    
-		        socketOut = new PrintWriter(connection.getOutputStream(), true);
-		        socketIn = new Scanner(new BufferedReader(new InputStreamReader(connection.getInputStream())));
-
-		        name =  connection.getInetAddress().getHostName();
-		        System.out.println("start chating");
-		        do{	        			        
-				  response = keyboardIn.nextLine();
-				  socketOut.println(nickname + ": " +response.toLowerCase());
-				  
-		          socketOut.flush();
-		          answer = socketIn.nextLine();
-		          System.out.println(answer);
-		        }
-		        while(!response.equalsIgnoreCase("exit"));
-		        System.out.println("Closing the connection with " + name);
-		      }
-		      catch(IOException e) {
+		        socketOut.println(nickname + ": " + " hello ");
+		        do{	  
+		          receiveMessage();
+		          sendMessage(keyboardIn);				  	        
+		        } while(!response.equalsIgnoreCase("exit"));
+		        System.out.println("Closing the connection with " + computerName);
+		      } catch(IOException e) {
 		        e.printStackTrace();
 		      }
 		      finally{
 		        try{
-		          if (socketIn!=null) socketIn.close();
-		          if (socketOut!=null) socketOut.close();
-		          if (connection!=null) connection.close();
-		          if (serverSocket!=null) serverSocket.close();
-		        }
-		        catch(IOException e){
+		          closeTheSockets();
+		        } catch(IOException e){
 		          System.err.println(" Can not close the socket! ");
 		        }
-		      }
-		  }
+		     }
+		 }
 	 }
+
+	private static void closeTheSockets() throws IOException {
+		if (socketIn!=null) socketIn.close();
+		if (socketOut!=null) socketOut.close();
+	    if (connection!=null) connection.close();
+		if (serverSocket!=null) serverSocket.close();
+	}
+
+	private static void receiveMessage() {
+		socketOut.flush();
+		answer = socketIn.nextLine();
+	    System.out.println(answer);
+	}
+
+	private static void sendMessage(Scanner keyboardIn) {
+		System.out.print("Me: ");
+	    response = keyboardIn.nextLine();
+		socketOut.println(nickname + ": " +response.toLowerCase());
+	}
+
+	private static void setStreams() throws IOException {
+		socketOut = new PrintWriter(connection.getOutputStream(), true);
+		socketIn = new Scanner(new BufferedReader(new InputStreamReader(connection.getInputStream())));
+	}
+
+	private static void setingNickname(Scanner keyboardIn) {
+		System.out.println(" Please enter your nickname!" );
+		nickname = keyboardIn.nextLine();
+	}
+
+	private static void WaitinForConnection() throws IOException {
+		serverSocket = new ServerSocket(PORT);
+		System.out.println(" Waiting for someone to connect...");
+		connection = serverSocket.accept();
+		computerName =  connection.getInetAddress().getHostName();
+		System.out.println(" Connected with:" + computerName);
+	}
 }
